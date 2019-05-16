06d3303efab7269cfe1c63a33980cd7aa9f47b58
Shaun <shaun@thenetninja.co.uk>
2019-05-16 21:14:04 +0100

    lesson-3

--- contextapp/src/contexts/ThemeContext.js
+++ contextapp/src/contexts/ThemeContext.js
@@ -6,7 +6,7 @@ class ThemeContextProvider extends Component {
   state = {
     isLightTheme: true,
     light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
-    dark: { syntax: '#ddd', ui: '#333', bg: '#eee'}
+    dark: { syntax: '#ddd', ui: '#333', bg: '#555'}
   }
   render() { 
     return (
