36b17f13d0ac54ceb75207e45b65f6e40b5c1a8c
Shaun <shaun@thenetninja.co.uk>
2019-05-17 11:35:11 +0100

    lesson-13

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return ( 
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
+// class BookList extends Component {
+//   static contextType = ThemeContext;
+//   render() { 
+//     const { isLightTheme, light, dark } = this.context;
+//     const theme = isLightTheme ? light : dark;
+//     return ( 
+//       <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+//         <ul>
+//           <li style={{ background: theme.ui }}>the way of kings</li>
+//           <li style={{ background: theme.ui }}>the name of the wind</li>
+//           <li style={{ background: theme.ui }}>the final empire</li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return ( 
+    <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  );
 }
  
 export default BookList;
