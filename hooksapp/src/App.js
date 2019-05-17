03ca197ca70dc658e76557e8756294a0ca4707bb
Shaun <shaun@thenetninja.co.uk>
2019-05-17 10:08:46 +0100

    lesson-9

---
+++ hooksapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SongList from './components/SongList';
+
+function App() {
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
+}
+
+export default App;
---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SongList;
