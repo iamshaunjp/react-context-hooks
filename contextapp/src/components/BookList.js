3934bdb9eca19fef573ca48951b229fe70c57b16
Rahul Trivedi <rahultrivedi180@gmail.com>
2021-07-12 20:18:39 +0530

    Navbar, Booklist ThemeContext consumer as a functional component

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return ( 
-      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
- 
+const BookList = () => {
+  return (
+    <ThemeContext.Consumer>
+      {(themeContext) => {
+        const theme = themeContext.isLightTheme
+          ? themeContext.light
+          : themeContext.dark;
+
+        return (
+          <div
+            className='book-list'
+            style={{ color: theme.syntax, background: theme.bg }}
+          >
+            <ul>
+              <li style={{ background: theme.ui }}>the way of kings</li>
+              <li style={{ background: theme.ui }}>the name of the wind</li>
+              <li style={{ background: theme.ui }}>the final empire</li>
+            </ul>
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
+
 export default BookList;
